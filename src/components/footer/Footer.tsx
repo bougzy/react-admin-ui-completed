0b17d796f52b7a8ba4f7564c142adb0c5066db76
PatrickSpecial <47539464+PatrickSpecial@users.noreply.github.com>
2023-08-13 19:26:04 +0100

    Admin Dashboard

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>Bougzy</span>
+      <span>© Bougzy Dev Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
